ba9a1df34aa7787201294fe01eaaf54c84da4920
anand-coditas <anand.kumar@coditas.com>
2022-12-18 02:33:04 +0530

    Basic UI

--- CalculatorAndroidApp/build.gradle.kts
+++ CalculatorAndroidApp/build.gradle.kts
@@ -19,6 +19,7 @@ android {
             isMinifyEnabled = false
         }
     }
+
 }
 
 dependencies {
--- CalculatorAndroidApp/src/main/java/com/example/calculatorinkmm/android/MainActivity.kt
+++ CalculatorAndroidApp/src/main/java/com/example/calculatorinkmm/android/MainActivity.kt
@@ -2,39 +2,12 @@ package com.example.calculatorinkmm.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.EditText
-import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val numATV: EditText = findViewById(R.id.editTextNumberDecimalA)
-        val numBTV: EditText = findViewById(R.id.editTextNumberDecimalB)
-
-        val sumTV: TextView = findViewById(R.id.textViewSum)
-
-        val textWatcher = object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                try {
-                    val numA = Integer.parseInt(numATV.text.toString())
-                    val numB = Integer.parseInt(numBTV.text.toString())
-                    sumTV.text =  "= " + numA+numB
-                } catch(e: NumberFormatException) {
-                    sumTV.text = "= 🤔"
-                }
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        }
-
-        numATV.addTextChangedListener(textWatcher)
-        numBTV.addTextChangedListener(textWatcher)
 
     }
 }
